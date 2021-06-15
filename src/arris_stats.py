60db0bbd13ebf04612dbf7a46875d6e0af18eb4b
Micah Huff <mphuff@amazon.com>
2021-06-14 21:40:00 -0700

    Support Arris SB6183

--- src/arris_stats.py
+++ src/arris_stats.py
@@ -83,6 +83,8 @@ def main():
         # Parse the HTML to get our stats
         if modem_model == 'sb8200':
             stats = parse_html_sb8200(html)
+        elif modem_model == 'sb6183':
+            stats = parse_html_sb6183(html)
         else:
             error_exit('Modem model %s not supported!  Aborting', sleep=False)
 
@@ -101,7 +103,7 @@ def main():
 def get_args():
     """ Get argparser args """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', metavar='config_file_path', help='Path to config file', required=True)
+    parser.add_argument('--config', metavar='config_file_path', help='Path to config file', required=False)
     parser.add_argument('--debug', help='Enable debug logging', action='store_true', required=False, default=False)
     args = parser.parse_args()
     return args
@@ -118,12 +120,12 @@ def get_config(config_path=None):
         'arris_stats_debug': False,
         'destination': 'influxdb',
         'sleep_interval': 300,
-        'modem_url': 'https://192.168.100.1/cmconnectionstatus.html',
+        'modem_url': 'http://192.168.100.1/RgConnect.asp',
         'modem_verify_ssl': False,
         'modem_auth_required': False,
         'modem_username': 'admin',
         'modem_password': None,
-        'modem_model': 'sb8200',
+        'modem_model': 'sb6183',
         'exit_on_auth_error': True,
         'exit_on_html_error': True,
         'clear_auth_token_on_html_error': True,
@@ -332,6 +334,105 @@ def parse_html_sb8200(html):
 
     return stats
 
+
+def parse_html_sb6183(html):
+    """ Parse the HTML into the modem stats dict """
+    logging.info('Parsing HTML for modem model sb6183')
+
+    # Page to parse: http://192.168.100.1/RgConnect.asp
+
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(html, 'html.parser')
+    stats = {}
+
+    # downstream table
+    stats['downstream'] = []
+    logging.debug("Found %s tables" % len(soup.find_all("table")))
+    for table_row in soup.find_all("table")[2].find_all("tr"):
+        if table_row.th:
+            continue
+
+        '''
+        <tr>
+    <td ><strong>Channel</strong></td>
+    <td ><strong>Lock Status</strong></td>
+    <td ><strong>Modulation</strong></td>
+    <td ><strong>Channel ID</strong></td>
+    <td ><strong>Frequency</strong></td>
+    <td ><strong>Power</strong></td>
+    <td ><strong>SNR</strong></td>
+    <td ><strong>Corrected</strong></td>
+    <td ><strong>Uncorrectables</strong></td>
+   </tr>
+        '''
+
+        channel = table_row.find_all('td')[0].text.strip()
+        logging.debug("Processing downstream channel %s" % channel)
+        # Some firmwares have a header row not already skiped by "if table_row.th", skip it if channel_id isn't an integer
+        if not channel.isdigit():
+            continue
+
+        channel_id = table_row.find_all('td')[3].text.strip()
+        frequency = table_row.find_all('td')[4].text.replace(" Hz", "").strip()
+        power = table_row.find_all('td')[5].text.replace(" dBmV", "").strip()
+        snr = table_row.find_all('td')[6].text.replace(" dB", "").strip()
+        corrected = table_row.find_all('td')[7].text.strip()
+        uncorrectables = table_row.find_all('td')[8].text.strip()
+
+        stats['downstream'].append({
+            'channel': channel,
+            'channel_id': channel_id,
+            'frequency': frequency,
+            'power': power,
+            'snr': snr,
+            'corrected': corrected,
+            'uncorrectables': uncorrectables
+        })
+
+    logging.debug('downstream stats: %s', stats['downstream'])
+    if not stats['downstream']:
+        logging.error('Failed to get any downstream stats! Probably a parsing issue in parse_html_sb8200()')
+
+    # upstream table
+    stats['upstream'] = []
+    for table_row in soup.find_all("table")[3].find_all("tr"):
+        if table_row.th:
+            continue
+
+        '''
+        <tr>
+            <td><strong>Channel</strong></td>
+            <td><strong>Lock Status</strong></td>
+                        <td><strong>US Channel Type</strong></td>
+                        <td><strong>Channel ID</strong></td>
+                        <td><strong>Symbol Rate</strong></td>
+                        <td><strong>Frequency</strong></td>
+                        <td><strong>Power</strong></td>
+           </tr>
+        '''
+
+        # Some firmwares have a header row not already skiped by "if table_row.th", skip it if channel_id isn't an integer
+        channel = table_row.find_all('td')[0].text.strip()
+        if not channel.isdigit():
+            continue
+
+        channel_id = table_row.find_all('td')[3].text.strip()
+        symbol_rate = table_row.find_all('td')[4].text.replace(" Ksym/sec", "").strip()
+        frequency = table_row.find_all('td')[5].text.replace(" Hz", "").strip()
+        power = table_row.find_all('td')[6].text.replace(" dBmV", "").strip()
+
+        stats['upstream'].append({
+            'channel_id': channel_id,
+            'symbol_rate': symbol_rate,
+            'frequency': frequency,
+            'power': power,
+        })
+
+    logging.debug('upstream stats: %s', stats['upstream'])
+    if not stats['upstream']:
+        logging.error('Failed to get any upstream stats! Probably a parsing issue in parse_html_sb8200()')
+
+    return stats
 
 def send_to_influx(stats, config):
     """ Send the stats to InfluxDB """
