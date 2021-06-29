1205d1bf4e817bd042a178a91f810d5cdc7bee17
Andy Fraley <andrew.fraley@gmail.com>
2021-06-29 02:28:48 -0400

    add more unit tests and split the model functions into separate files - WIP

--- src/arris_stats.py
+++ src/arris_stats.py
@@ -16,6 +16,7 @@ import configparser
 from datetime import datetime
 import urllib3
 import requests
+import arris_stats_sb8200 as sb8200
 
 HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -82,7 +83,7 @@ def main():
 
         # Parse the HTML to get our stats
         if modem_model == 'sb8200':
-            stats = parse_html_sb8200(html)
+            stats = sb8200.parse_html_sb8200(html)
         else:
             error_exit('Modem model %s not supported!  Aborting', sleep=False)
 
@@ -260,79 +261,6 @@ def get_html(config, credential):
     return status_html
 
 
-def parse_html_sb8200(html):
-    """ Parse the HTML into the modem stats dict """
-    logging.info('Parsing HTML for modem model sb8200')
-
-    # As of Aug 2019 the SB8200 has a bug in its HTML
-    # The tables have an extra </tr> in the table headers, we have to remove it so
-    # that Beautiful Soup can parse it
-    # Before: <tr><th colspan=7><strong>Upstream Bonded Channels</strong></th></tr>
-    # After: <tr><th colspan=7><strong>Upstream Bonded Channels</strong></th>
-    html = html.replace('Bonded Channels</strong></th></tr>', 'Bonded Channels</strong></th>', 2)
-
-    from bs4 import BeautifulSoup
-    soup = BeautifulSoup(html, 'html.parser')
-    stats = {}
-
-    # downstream table
-    stats['downstream'] = []
-    for table_row in soup.find_all("table")[1].find_all("tr"):
-        if table_row.th:
-            continue
-
-        channel_id = table_row.find_all('td')[0].text.strip()
-
-        # Some firmwares have a header row not already skiped by "if table_row.th", skip it if channel_id isn't an integer
-        if not channel_id.isdigit():
-            continue
-
-        frequency = table_row.find_all('td')[3].text.replace(" Hz", "").strip()
-        power = table_row.find_all('td')[4].text.replace(" dBmV", "").strip()
-        snr = table_row.find_all('td')[5].text.replace(" dB", "").strip()
-        corrected = table_row.find_all('td')[6].text.strip()
-        uncorrectables = table_row.find_all('td')[7].text.strip()
-
-        stats['downstream'].append({
-            'channel_id': channel_id,
-            'frequency': frequency,
-            'power': power,
-            'snr': snr,
-            'corrected': corrected,
-            'uncorrectables': uncorrectables
-        })
-
-    logging.debug('downstream stats: %s', stats['downstream'])
-    if not stats['downstream']:
-        logging.error('Failed to get any downstream stats! Probably a parsing issue in parse_html_sb8200()')
-
-    # upstream table
-    stats['upstream'] = []
-    for table_row in soup.find_all("table")[2].find_all("tr"):
-        if table_row.th:
-            continue
-
-        # Some firmwares have a header row not already skiped by "if table_row.th", skip it if channel_id isn't an integer
-        if not channel_id.isdigit():
-            continue
-
-        channel_id = table_row.find_all('td')[1].text.strip()
-        frequency = table_row.find_all('td')[4].text.replace(" Hz", "").strip()
-        power = table_row.find_all('td')[6].text.replace(" dBmV", "").strip()
-
-        stats['upstream'].append({
-            'channel_id': channel_id,
-            'frequency': frequency,
-            'power': power,
-        })
-
-    logging.debug('upstream stats: %s', stats['upstream'])
-    if not stats['upstream']:
-        logging.error('Failed to get any upstream stats! Probably a parsing issue in parse_html_sb8200()')
-
-    return stats
-
-
 def send_to_influx(stats, config):
     """ Send the stats to InfluxDB """
     logging.info('Sending stats to InfluxDB (%s:%s)', config['influx_host'], config['influx_port'])
@@ -452,7 +380,6 @@ def init_logger(debug=False):
         root_handler.setFormatter(logging.Formatter(log_format))
     except IndexError:
         logging.basicConfig(level=level, format=log_format)
-
 
 
 if __name__ == '__main__':
---
+++ src/arris_stats_sb8200.py
@@ -0,0 +1,79 @@
+"""
+    Functions for model SB8200
+    https://github.com/andrewfraley/arris_cable_modem_stats
+"""
+# pylint: disable=line-too-long
+import logging
+from bs4 import BeautifulSoup
+
+
+def parse_html_sb8200(html):
+    """ Parse the HTML into the modem stats dict """
+    logging.info('Parsing HTML for modem model sb8200')
+
+    # As of Aug 2019 the SB8200 has a bug in its HTML
+    # The tables have an extra </tr> in the table headers, we have to remove it so
+    # that Beautiful Soup can parse it
+    # Before: <tr><th colspan=7><strong>Upstream Bonded Channels</strong></th></tr>
+    # After: <tr><th colspan=7><strong>Upstream Bonded Channels</strong></th>
+    html = html.replace('Bonded Channels</strong></th></tr>', 'Bonded Channels</strong></th>', 2)
+
+    soup = BeautifulSoup(html, 'html.parser')
+    stats = {}
+
+    # downstream table
+    stats['downstream'] = []
+    for table_row in soup.find_all("table")[1].find_all("tr"):
+        if table_row.th:
+            continue
+
+        channel_id = table_row.find_all('td')[0].text.strip()
+
+        # Some firmwares have a header row not already skiped by "if table_row.th", skip it if channel_id isn't an integer
+        if not channel_id.isdigit():
+            continue
+
+        frequency = table_row.find_all('td')[3].text.replace(" Hz", "").strip()
+        power = table_row.find_all('td')[4].text.replace(" dBmV", "").strip()
+        snr = table_row.find_all('td')[5].text.replace(" dB", "").strip()
+        corrected = table_row.find_all('td')[6].text.strip()
+        uncorrectables = table_row.find_all('td')[7].text.strip()
+
+        stats['downstream'].append({
+            'channel_id': channel_id,
+            'frequency': frequency,
+            'power': power,
+            'snr': snr,
+            'corrected': corrected,
+            'uncorrectables': uncorrectables
+        })
+
+    logging.debug('downstream stats: %s', stats['downstream'])
+    if not stats['downstream']:
+        logging.error('Failed to get any downstream stats! Probably a parsing issue in parse_html_sb8200()')
+
+    # upstream table
+    stats['upstream'] = []
+    for table_row in soup.find_all("table")[2].find_all("tr"):
+        if table_row.th:
+            continue
+
+        # Some firmwares have a header row not already skiped by "if table_row.th", skip it if channel_id isn't an integer
+        if not channel_id.isdigit():
+            continue
+
+        channel_id = table_row.find_all('td')[1].text.strip()
+        frequency = table_row.find_all('td')[4].text.replace(" Hz", "").strip()
+        power = table_row.find_all('td')[6].text.replace(" dBmV", "").strip()
+
+        stats['upstream'].append({
+            'channel_id': channel_id,
+            'frequency': frequency,
+            'power': power,
+        })
+
+    logging.debug('upstream stats: %s', stats['upstream'])
+    if not stats['upstream']:
+        logging.error('Failed to get any upstream stats! Probably a parsing issue in parse_html_sb8200()')
+
+    return stats
